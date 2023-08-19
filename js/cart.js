594a3671422dd29c92d5b968cf400befb155882b
rootnure <nur.diu.2791@gmail.com>
2023-08-20 04:45:11 +0600

    Discount and Final price dynamic set

--- js/cart.js
+++ js/cart.js
@@ -33,4 +33,50 @@ function setTotalPrice(newItemPrice) {
     const newTotalPrice = previousTotalPrice + newItemPrice;
 
     totalPriceElement.innerText = newTotalPrice.toFixed(2) + 'TK';
+
+    if(newTotalPrice > 0) {
+        const element = pricesContainer.parentNode.childNodes[9];
+        toggleMakePurchaseBtn(element, true);
+
+        const discountPriceElement = pricesContainer.childNodes[3];
+        const finalPriceElement = pricesContainer.childNodes[5];
+        calculateDiscountAndFinalPrice(newTotalPrice, discountPriceElement, finalPriceElement);
+    }
+
+    if(newTotalPrice >= 200) {
+        toggleCouponApplyBtn(true);
+    }
+}
+
+const coupons = { 'SELL200': 20 };
+
+let discountPercentage = 0;
+
+function calculateDiscountAndFinalPrice(totalPrice, discountAmountElement, finalAmountElement) {
+    let discountAmount = 0;
+
+    const finalAmountString = finalAmountElement.childNodes[1].innerText.split('TK')[0];
+    let finalAmount = parseFloat(finalAmountString);
+
+    if(discountPercentage > 0) {
+        discountAmount = totalPrice * discountPercentage / 100;
+    }
+    
+    finalAmount = totalPrice - discountAmount;
+    discountAmountElement.childNodes[1].innerText = discountAmount.toFixed(2) + 'TK';
+
+    finalAmountElement.childNodes[1].innerText = finalAmount.toFixed(2) + ' TK';
+}
+
+function toggleMakePurchaseBtn(makePurchaseBtn, isActive) {
+    if(isActive) {
+        makePurchaseBtn.removeAttribute('disabled');
+    }
+}
+
+function toggleCouponApplyBtn(isActive) {
+    const couponApplyBtn = document.querySelector('#coupon-apply-btn');
+    if(isActive) {
+        couponApplyBtn.removeAttribute('disabled');
+    }
 }
